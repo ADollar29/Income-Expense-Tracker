fef1ec24d8dcb8c4c73f04a1c788491b209ec7ea
Abdalla Osman <99369800+AOsman29@users.noreply.github.com>
2024-02-10 17:18:36 -0700

    Feb changes
    Created the front end and added tailwind css features, login features and transaction details

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
+<<<<<<< Updated upstream
 function App(){
     return(
         <div className="App">
             <h1 className="text-green-200">Income-Expense Tracker</h1>
         </div>
     );
+=======
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Forms/Login';
+import HomePage from './components/HomePage/HomePage';
+import Register from './components/Forms/Register';
+import Navbar from './components/Navbar/Navbar';
+import AddTransaction from './components/Forms/AddTransaction';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/add-transaction" element={<AddTransaction/>} />
+    </Routes>
+    </BrowserRouter>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App;
---
+++ client/src/components/context/AuthContext/AuthContext.js
@@ -0,0 +1,52 @@
+import { createContext, useReducer }from "react";
+import axios from 'axios';
+//auth context
+
+export const authContext = createContext();
+
+const INITIAL_STATE = {
+    userAuth: null,
+    error: null,
+    loading: false,
+    profile: null,
+};
+//Create the Auth reducer
+
+const reducer = (state, action)=>{
+    return{};
+};
+//Provider (high order componenet)
+
+const AuthContextProvider = ({children}) =>{
+    const [state, dispatch] = useReducer((reducer, INITIAL_STATE));
+
+    //login action
+    const loginUserAction = async(formData)=>{
+        const config = {
+            header:{
+                "Content-Type":"application/json"
+            },
+        };
+    try {
+      const res = await axios.post(
+      "http://localhost:9000/api/v1/users/login", 
+      formData, 
+      config
+      );
+      console.log(res);
+    } catch (error) {
+        console.log(error);
+    } 
+    };
+    return(
+    <authContext.Provider 
+    value={{
+            loginUserAction,
+         }}
+    >
+        {children}
+    </authContext.Provider>
+    );
+};
+
+export default AuthContextProvider;
